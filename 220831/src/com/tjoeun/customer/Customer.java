195e1a52bb161abb6e6978dafa8207a406aaeb59
seon219 <69349276+seon219@users.noreply.github.com>
2022-09-01 09:44:43 +0900

    Add files via upload

---
+++ 220831/src/com/tjoeun/customer/Customer.java
@@ -0,0 +1,80 @@
+package com.tjoeun.customer;
+
+// 일반고객 정보를 기억하는 클래스
+public class Customer {
+
+	private int id;
+	private String name;
+	private String grade;
+	private int point;
+	private double ratio; // 포인트 적립 비율
+	
+	public Customer() {
+		grade = "SILVER";
+		ratio = 0.01;
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public double getRatio() {
+		return ratio;
+	}
+
+	public void setRatio(double ratio) {
+		this.ratio = ratio;
+	}
+
+	
+	// 고객 정보를 출력하는 메소드
+	public String showCustomerInfo() {
+		return name + "님의 등급은 " + grade + "이며, 보너스 포인트는 " + point +"점 입니다.";
+	}
+	
+	
+	// 구매 금액을 인수로 넘겨받아 보너스 포인트를 계산해서 리턴하는 메소드
+	public int calcBonus(int price) {
+		return (int) (price * ratio);
+	}
+	
+	
+	// 구매 금액을 인수로 넘겨받아 누적 보너스 포인트를 계산해서 리턴하는 메소드
+	public int calcPrice(int price) {
+		//point += price * ratio;
+		point += calcBonus(price);
+		return point;
+	}
+	
+	
+	
+}
